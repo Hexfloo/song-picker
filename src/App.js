1d9d3d403feec97a3203a1dc72b63498b64c2c24
hexfloo <shoham.palmer@gmail.com>
2023-01-30 19:17:12 +0200

    Finish logic for didVote, addVote in App component

--- src/App.js
+++ src/App.js
@@ -5,18 +5,26 @@ import Introduction from "./components/Introduction";
 import Thanks from "./components/Thanks";
 import Dj from "./components/Dj";
 
-const INITIAL_VOTES = [{ song1: 1 }, { song2: 1 }, { song3: 1 }];
+const INITIAL_VOTES = [
+  { id: "song1", votes: 1 },
+  { id: "song2", votes: 1 },
+  { id: "song3", votes: 1 },
+];
 
 function App() {
   const [votes, setVotes] = useState(INITIAL_VOTES);
   const [didVote, setDidVote] = useState(false);
 
   const addVote = function (newVote) {
-    setVotes();
-
+    const newVotesObj = votes.map((song) => {
+      if (song.id === newVote) {
+        song.votes++;
+      }
+    });
+    setVotes(newVotesObj);
+    setDidVote(true);
     //setDidVote(true); ADD IT BACK WHEN FINISHED!!!!!@#$%^&*(*&^%$#@)
     console.log("set did vote to true and add localStorage");
-    console.log(votes);
     localStorage.setItem("ifVoted", "true");
   };
   return (
--- src/components/Dj.js
+++ src/components/Dj.js
@@ -2,21 +2,21 @@ import "../App.css";
 const Dj = function (props) {
   return (
     <>
-      <div class="container my-4 box">
+      <div className="container my-4 box">
         <h1>Voting Results</h1>
         <br />
 
-        <h5>Sunshine of my life: {props.items[0].song1}</h5>
+        <h5>Sunshine of my life: {props.items[0].votes}</h5>
 
-        <h5>{props.items[1].song2} :שלמים</h5>
+        <h5>{props.items[1].votes} :שלמים</h5>
 
-        <h5>The one that I want: {props.items[2].song3}</h5>
+        <h5>The one that I want: {props.items[2].votes}</h5>
 
         <a href="/">
-          <button class="btn submit-btn-on shadow-none">Home</button>
+          <button className="btn submit-btn-on shadow-none">Home</button>
         </a>
       </div>
-      <div class="m-0 leaf-box">
+      <div className="m-0 leaf-box">
         <img
           src="https://res.cloudinary.com/hexfloo/image/upload/v1650201485/morandstav/leaf_ccso8c.png"
           alt="leaf"
